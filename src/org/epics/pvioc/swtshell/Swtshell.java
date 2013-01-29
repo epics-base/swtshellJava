081646babacd447394d44ca91c94f04a75e4ec54
Marty Kraimer <mrkraimer@comcast.net>
2013-01-29 10:25:53 -0500

    copied swtshell from pvIOCJava; when working will remove from pvIOCJava

---
+++ src/org/epics/pvioc/swtshell/Swtshell.java
@@ -0,0 +1,13 @@
+package org.epics.pvioc.swtshell;
+
+public class Swtshell {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SwtshellFactory.swtshell();
+        return;
+    }
+
+}
