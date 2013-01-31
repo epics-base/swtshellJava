a3cd75dd4d91628cd8950c73ad46ff8c01d67f6f
Marty Kraimer <mrkraimer@comcast.net>
2013-01-31 07:46:34 -0500

    start pvAccess

--- src/org/epics/pvioc/swtshell/Swtshell.java
+++ src/org/epics/pvioc/swtshell/Swtshell.java
@@ -6,6 +6,8 @@ public class Swtshell {
      * @param args
      */
     public static void main(String[] args) {
+        org.epics.pvaccess.ClientFactory.start();
+        org.epics.caV3.ClientFactory.start();
         SwtshellFactory.swtshell();
         return;
     }
